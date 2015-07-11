986086ca4f1dace4d8f4dbb4447e600f7cf9bb7d
Aaron M Shoemaker <ashoemaker@precisiondev.net>
2015-07-10 20:17:37 -0700

    Updated logstash_server.rb to include log parsing for our standard format. Whitespace correction in cloudformation.vpc_integration.Template.

--- site-cookbooks/bb_monitor/attributes/logstash_server.rb
+++ site-cookbooks/bb_monitor/attributes/logstash_server.rb
@@ -25,6 +25,19 @@ normal[:logstash][:server][:inputs] = [
   }
 ]
 
+normal[:logstash][:server][:filters] = [
+  {
+    "grok"=> {
+      "match" => {
+        "message" => "\[%{GREEDYDATA:project}\] %{TIMESTAMP_ISO8601:utcdate} %{LOGLEVEL:loglevel} %{NOTSPACE:classname} :: %{GREEDYDATA:messagetext} ## %{GREEDYDATA:exception}"
+      },
+      "remove_field" => [
+        "message"
+      ]
+    }
+  }
+]
+
 normal[:logstash][:server][:outputs] = [
   {
     "elasticsearch"=> {
