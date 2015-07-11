55a9cfd05f22b20c7d40ad68be89563affeb5b11
Aaron M Shoemaker <ashoemaker@precisiondev.net>
2015-07-10 22:38:04 -0700

    Attempted fix of Logstash grok configuration.

--- site-cookbooks/bb_monitor/attributes/logstash_server.rb
+++ site-cookbooks/bb_monitor/attributes/logstash_server.rb
@@ -25,12 +25,17 @@ normal[:logstash][:server][:inputs] = [
   }
 ]
 
-normal[:logstash][:server][:filters][:grok] = [
-  "match" => {
-    "message" => "\[%{GREEDYDATA:project}\] %{TIMESTAMP_ISO8601:utcdate} %{LOGLEVEL:loglevel} %{NOTSPACE:classname} :: %{GREEDYDATA:messagetext} ## %{GREEDYDATA:exception}"
-#  },
-#  {
-#    "remove_field" => "message"
+normal[:logstash][:server][:filters] = [
+  {
+    "grok"=> {
+      "type" => "nginx_access",
+      "match" => [
+        "message", "\[%{GREEDYDATA:project}\] %{TIMESTAMP_ISO8601:utcdate} %{LOGLEVEL:loglevel} %{NOTSPACE:classname} :: %{GREEDYDATA:messagetext} ## %{GREEDYDATA:exception}"
+      ]
+    },
+    "remove_field" => [
+      "message"
+    ]
   }
 ]
 
